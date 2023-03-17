c405c4a97a1471fad505ded8ba3f686fa13a1b6a
RobertoZefi <zefi.roberto2001@gmail.com>
2023-03-17 17:29:45 +0100

    FEAT es. done

--- badWords.php
+++ badWords.php
@@ -1,11 +1,17 @@
 <?php
 
 $par = $_GET ['par'];
+$cens = $_GET ['cens']
 
 ?>
 
-<h1> <?php echo $par ?></h1>
+<h1>Paragrafo: <?php echo $par ?></h1>
+<h2>Lunghezza paragrafo: <?php echo strlen($par) ?> </h2>
 
 <?php
 
-echo strlen($par);
+$new_par = str_replace($cens, "***", $par);
+
+?>
+
+<h2>Paragrafo con parola censurata: <?php echo $new_par ?></h2>
